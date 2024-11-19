2e61679f904087d020c4401ed3e2fddf05fd718c
Nicole Droege <nicole.droege@mubea.com>
2024-11-19 11:29:07 +0100

    done 10 EUR x 2 = 20 EUR

--- Code_Nicole/js/test_money.js
+++ Code_Nicole/js/test_money.js
@@ -6,7 +6,7 @@ Node.js v14 ("Fermium") or v16
 
 
 // done 5 USD x 2 = 10 USD
-// todo 10 EUR x 2 = 20 EUR
+// done 10 EUR x 2 = 20 EUR
 // todo 4002 KRW / 4 = 1000.5 KRW
 // todo 5 USD + 10 EUR = 17 USE (if exchanging 1 EUR gets us 1.2 USD)
 // todo 1 USD + 1100 KRW = 2200 KRW (if exchanging 1 USD gets us 1100 KRW)
