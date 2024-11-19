9e1ca79721be49a27e769d1641da23a81f3029e3
Nicole Droege <nicole.droege@mubea.com>
2024-11-19 10:53:43 +0100

    First green test after cleanup and first RGR (red-gren-refactor) cycle without code duplication, also in go and js

---
+++ Code_Nicole/js/test_money.js
@@ -0,0 +1,17 @@
+// Saleem Siddiqui: Learning Test-Driven Development, O'Reilly, 2023
+
+const assert = require('assert');
+
+class Dollar{
+	constructor(amount){
+		this.amount = amount;
+	}
+	
+	times (multiplier){
+		return new Dollar(this.amount * multiplier);
+	}
+}
+
+let fiver = new Dollar(5);
+let tenner = fiver.times(2);
+assert.strictEqual(tenner.amount, 10);
