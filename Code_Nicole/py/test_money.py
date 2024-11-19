2169fc27a92ad497d0b20d41c3057cc9ef007449
Nicole Droege <nicole.droege@mubea.com>
2024-11-19 10:19:29 +0100

    First green test

--- Code_Nicole/py/test_money.py
+++ Code_Nicole/py/test_money.py
@@ -5,11 +5,19 @@ Saleem Siddiqui: Learning Test-Driven Development, O'Reilly, 2023
 import unittest
 
 
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount
+
+    def times(self, multiplier):
+        return Dollar(10)
+
+
 class TestMoney(unittest.TestCase):
     def testMultiplication(self):
         fiver = Dollar(5)
         tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amout)
+        self.assertEqual(10, tenner.amount)
 
 
 if __name__ == '__main__':
