6f0c772649ae233e84aa627415e832265ffe5a58
Nicole Droege <nicole.droege@mubea.com>
2024-11-19 11:18:34 +0100

    Green tests for Money class and Euro multiplication

--- Code_Nicole/py/test_money.py
+++ Code_Nicole/py/test_money.py
@@ -14,6 +14,16 @@ import unittest
 # todo 5 USD + 10 EUR = 17 USE (if exchanging 1 EUR gets us 1.2 USD)
 # todo 1 USD + 1100 KRW = 2200 KRW (if exchanging 1 USD gets us 1100 KRW)
 
+
+class Money:
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
+
+    def times(self, multiplier):
+        return Money(self.amount * multiplier, self.currency)
+
+
 class Dollar:
     def __init__(self, amount):
         self.amount = amount
@@ -27,6 +37,12 @@ class TestMoney(unittest.TestCase):
         fiver = Dollar(5)
         tenner = fiver.times(2)
         self.assertEqual(10, tenner.amount)  # first the target, then the calculated value
+
+    def testMultiplicationInEuros(self):
+        ten_euros = Money(10, "EUR")
+        twenty_euros = ten_euros.times(2)
+        self.assertEqual(20, twenty_euros.amount)
+        self.assertEqual("EUR", twenty_euros.currency)
 
 
 if __name__ == '__main__':
