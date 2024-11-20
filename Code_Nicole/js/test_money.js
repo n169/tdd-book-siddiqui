e0ff8a8f7383ca7be539e8d45affeb0bd235bcd5
Nicole Droege <nicole.droege@mubea.com>
2024-11-20 13:44:46 +0100

    Separated js code to modules; improved tests.

--- Code_Nicole/js/test_money.js
+++ Code_Nicole/js/test_money.js
@@ -9,61 +9,67 @@ Node.js v14 ("Fermium") or v16
 // done 10 EUR x 2 = 20 EUR
 // done 4002 KRW / 4 = 1000.5 KRW
 // done 5 USD + 10 USD = 15 USD (adding Money's in same currency)
-// todo Separate test code from production code
+// done Separate test code from production code
 // todo Remove redundant tests
 // todo 5 USD + 10 EUR = 17 USD (if exchanging 1 EUR gets us 1.2 USD)
 // todo 1 USD + 1100 KRW = 2200 KRW (if exchanging 1 USD gets us 1100 KRW)
 
 
 const assert = require('assert');
+const Money = require('./money');
+const Portfolio = require('./portfolio');
 
-class Money{
-    constructor(amount, currency){
-        this.amount = amount;
-        this.currency = currency;
+class MoneyTest{
+    testMultiplication(){
+        let tenEuros = new Money(10, "EUR");
+        let twentyEuros = new Money(20, "EUR");
+        assert.deepStrictEqual(twentyEuros, tenEuros.times(2));
     }
 
-    times(multiplier){
-        return new Money(this.amount * multiplier, this.currency);
+    testDivision(){
+        let originalMoney = Money(4002, "KRW");
+        let actualMoneyAfterDivision = originalMoney.divide(4);
+        let expectedMoneyAfterDivision = Money(1000.5, "KRW");
+        assert.deepStrictEqual(expectedMoneyAfterDivision, actualMoneyAfterDivision);
     }
 
-    divide(divisor){
-        return new Money(this.amount / divisor, this.currency);
+    testAddition(){
+        let fiveDollars = new Money(5, "USD");
+        let tenDollars = new Money(10, "USD");
+        let fifteenDollars = new Money(15, "USD");
+        let portfolio = new Portfolio();
+        portfolio.add(fiveDollars, tenDollars);
+        assert.strictEqual(fifteenDollars, portfolio.evaluate("USD"));
+    }
+
+    getAllTestMethods(){
+        //let testMethods := ['testMultiplication', 'testDivision', 'testAddition'];
+
+        let moneyPrototype = MoneyTest.prototype; //JavaScript is prototype-based, not class-based
+        let allProps = Object.getOwnPropertyNames(moneyPrototype);
+        let testMethods = allProps.filter( p => {
+            return typeof moneyPrototype[p] === 'function' && p.startsWith("test");
+        });
+
+        return testMethods;
+    }
+
+    runAllTests(){
+        let testMethods := this.getAllTestMethods();
+        testMethods.forEach(m => {
+            console.log("Running: %s()", m); //Print the name of the method before invoking it
+            let method = Reflect.get(this, m); //Get the method object for each test method name via reflection
+            try {
+                Reflect.apply(method, this, []); //Invoke the test method with no arguments on this object
+            } catch (e) {
+                if (e instanceof assert.AssertionError) {
+                    console.log(e);
+                } else {
+                    throw e;
+                }
+            }
+        });
     }
 }
 
-class Portfolio{
-    constructor(){
-        this.moneys = [];
-    }
-
-    add(...moneys){
-        //the rest parameter syntax "..." allows multiple Money's to be added simultaneously
-        this.moneys = this.moneys.concat(moneys)
-    }
-
-    evaluate(currency){
-        let total = this.moneys.reduce( (sum, money) => {
-                        return sum + money.amount;
-                     }, 0);
-        return new Money(total, currency);
-    }
-}
-
-let fiveDollars = new Money(5, "USD");
-let tenDollars = new Money(10, "USD");
-assert.strictEqual(tenDollars, fiveDollars.times(2));
-
-let tenEuros = new Money(10, "EUR");
-let twentyEuros = new Money(20, "EUR");
-assert.deepStrictEqual(twentyEuros, tenEuros.times(2));
-
-let originalMoney = Money(4002, "KRW");
-let actualMoneyAfterDivision = originalMoney.divide(4);
-let expectedMoneyAfterDivision = Money(1000.5, "KRW");
-assert.deepStrictEqual(expectedMoneyAfterDivision, actualMoneyAfterDivision);
-
-let fifteenDollars = new Money(15, "USD");
-let portfolio = new Portfolio();
-portfolio.add(fiveDollars, tenDollars);
-assert.strictEqual(fifteenDollars, portfolio.evaluate("USD"));
+new MoneyTest().runAllTests();
