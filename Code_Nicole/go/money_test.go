c988f114b20717b40a22571207b93798c2fb0383
Nicole Droege <nicole.droege@mubea.com>
2024-11-20 13:28:35 +0100

    Separate Go code into modules, make amount/currency just accessible from within Money, create public NewMoney function, remove redundant tests. Also added the go.mod file.

--- Code_Nicole/go/money_test.go
+++ Code_Nicole/go/money_test.go
@@ -9,8 +9,10 @@ Go version 1.17
 // done 10 EUR x 2 = 20 EUR
 // done 4002 KRW / 4 = 1000.5 KRW
 // done 5 USD + 10 USD = 15 USD (adding Money's in same currency)
-// todo Separate test code from production code
-// todo Remove redundant tests
+// done Separate test code from production code
+// done Make amount and currency accessible only from within Money struct and not from outside
+// done Create a public New function to initialize the Money struct
+// done Remove redundant tests
 // todo 5 USD + 10 EUR = 17 USD (if exchanging 1 EUR gets us 1.2 USD)
 // todo 1 USD + 1100 KRW = 2200 KRW (if exchanging 1 USD gets us 1100 KRW)
 
@@ -19,73 +21,43 @@ package main
 
 import (
 	"testing"
+	s "tdd/stocks" //give "tdd/stocks" packages the variable name "s"
 )
 
-func assertEqual(t *testing.T, expected Money, actual Money){
+func assertEqual(t *testing.T, expected s.Money, actual s.Money){
     if expected != actual {
         t.Errorf("Expected %+v, Got %+v", expected, actual)
     }
 }
 
 func TestMultiplication(t *testing.T){
-	fiveDollars := Money{amount: 5, "USD"}
-	tenDollars := Money{amount: 10, "USD"}
-	assertEqual(t, tenDollars, fiveDollars.times(2))
-}
-
-func TestMultiplicationInEuros(t *testing.T){
-    tenEuros := Money{amount: 10, currency: "EUR"}
-    twentyEuros := Money{amount: 20, currency: "EUR"}
+    tenEuros := s.NewMoney{amount: 10, currency: "EUR"}
+    twentyEuros := s.NewMoney{amount: 20, currency: "EUR"}
     assertEqual(t, twentyEuros, tenEuros.times(2))
 }
 
 func TestDivision(t *testing.T){
-    originalMoney := Money{amount: 4002, currency: "KRW"}
+    originalMoney := s.NewMoney{amount: 4002, currency: "KRW"}
     actualResult := originalMoney.divide(4)
-    expectedResult := Money{amount: 1000.5, currency: "KRW"}
+    expectedResult := s.NewMoney{amount: 1000.5, currency: "KRW"}
     assertEqual(t, expectedResult, actualResult)
 }
 
 func TestAddition(t *testing.T){
-    var portfolio Portfolio
-    var portfolioInDollars Money
+    var portfolio s.Portfolio
+    var portfolioInDollars s.Money
 
-	fiveDollars := Money{amount: 5, "USD"}
-	tenDollars := Money{amount: 10, "USD"}
-	fifteenDollars := Money{amount: 15, "USD"}
+	fiveDollars := s.NewMoney{amount: 5, "USD"}
+	tenDollars := s.NewMoney{amount: 10, "USD"}
+	fifteenDollars := s.NewMoney{amount: 15, "USD"}
 
-	portfolio = Portfolio.add(fiveDollars)
-	portfolio = Portfolio.add(tenDollars)
+	portfolio = s.Portfolio.add(fiveDollars)
+	portfolio = s.Portfolio.add(tenDollars)
 	portfolioInDollars = portfolio.Evaluate("USD")
 
 	assertEqual(t, fifteenDollars, portfolioInDollars)
 }
 
 
-type Money struct{
-    amount float64
-    currency string
-}
 
-func (m Money) Times(multiplier int) Money{
-	return Money{amount: m.amount * float64(multiplier), currency: m.currency}
-}
 
-func (m Money) Divide(divisor int) Money{
-	return Money{amount: m.amount / float64(divisor), currency: m.currency}
-}
-
-type Portfolio []Money
-
-func (p Portfolio) Add(money Money) Money {
-    p = append(p, money)
-    return p
-}
-
-func (p Portfolio) Evaluate(currency string) Money {
-    total := 0.0
-    for _, m := range p {
-        total = total + m.amount
-    }
-    return Money{amount: total, currency: "USD"}
-}
